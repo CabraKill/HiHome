8008df0ef1e8557a7d8434486f48e7e2df299936
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-19 16:26:18 -0300

    Add state check to

--- lib/modules/splash/widgets/house_widget.dart
+++ lib/modules/splash/widgets/house_widget.dart
@@ -8,19 +8,27 @@ class House extends StatefulWidget {
 }
 
 class _HouseState extends State<House> with TickerProviderStateMixin {
+  bool state = true;
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: AnimatedSize(
-      curve: Curves.easeOut,
-      duration: Duration(milliseconds: 300),
-      vsync: this,
-      child: Container(
-        margin: const EdgeInsets.all(8),
-        alignment: Alignment.center,
-        color: Colors.red,
-        child: Text("nome"),
+      child: Center(
+        child: GestureDetector(
+          onTap: () => setState(() => state = !state),
+          child: AnimatedSize(
+            curve: Curves.easeOut,
+            duration: Duration(milliseconds: 300),
+            vsync: this,
+            child: Container(
+              margin: const EdgeInsets.all(8),
+              width: state ? 100 : null,
+              alignment: Alignment.center,
+              color: Colors.red,
+              child: Text("nome"),
+            ),
+          ),
+        ),
       ),
-    ));
+    );
   }
 }
