ba6b095af16d81d76e43c9bdcefcd8e8998d98b2
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-20 13:10:19 -0300

    feat(pages): add home details to pages

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,4 +1,7 @@
 import 'package:get/get.dart';
+import 'package:hihome/modules/details/details_biding.dart';
+import 'package:hihome/modules/details/details_controller.dart';
+import 'package:hihome/modules/details/details_page.dart';
 import 'package:hihome/modules/home/home_binding.dart';
 import 'package:hihome/modules/home/home_page.dart';
 import 'package:hihome/modules/splash/splash_biding.dart';
@@ -12,6 +15,10 @@ abstract class AppPages {
         name: Routes.SPLASH,
         page: () => SplashPage(),
         binding: SplashBinding()),
-    GetPage(name: Routes.HOME, page: () => HomePage(), binding: HomeBinding())
+    GetPage(name: Routes.HOME, page: () => HomePage(), binding: HomeBinding()),
+    GetPage(
+        name: Routes.HOME_DETAILS,
+        page: () => DetailsPage(),
+        binding: DetailsBinding()),
   ];
 }
