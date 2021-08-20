5176dfbab3bc610fa1e9115a0f8d22022cb611e5
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-20 10:39:14 -0300

    Add Homn to pages

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,4 +1,7 @@
 import 'package:get/get.dart';
+import 'package:hihome/modules/home/home_binding.dart';
+import 'package:hihome/modules/home/home_page.dart';
+import 'package:hihome/modules/splash/splash_biding.dart';
 import 'package:hihome/modules/splash/splash_page.dart';
 
 import 'routes.dart';
@@ -6,8 +9,9 @@ import 'routes.dart';
 abstract class AppPages {
   static final pages = [
     GetPage(
-      name: Routes.SPLASH,
-      page: () => SplashPage(),
-    )
+        name: Routes.SPLASH,
+        page: () => SplashPage(),
+        binding: SplashBinding()),
+    GetPage(name: Routes.HOME, page: () => HomePage(), binding: HomeBinding())
   ];
 }
