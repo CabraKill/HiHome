27fcdef6f7ff48918bd9c5c311b001029af6a353
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-20 13:08:33 -0300

    feat(firebaseMock): add a house to list

--- lib/data/provider/firebase/mock/firebaseMock.dart
+++ lib/data/provider/firebase/mock/firebaseMock.dart
@@ -3,7 +3,8 @@ import 'package:hihome/data/provider/firebase/firebase_interface.dart';
 
 class FirebaseMock implements FireBase {
   static final houseList = <HouseModel>[
-    HouseModel(id: "23412", name: "Netuno Galáxia Club")
+    HouseModel(id: "23412", name: "Netuno Galáxia Club"),
+    HouseModel(id: "23412", name: "Marte Galáxia Club"),
   ];
   @override
   Future<List<HouseModel>> getHomeList() async {
