97ec89912d369c24ae5815b39ebe3cdb8f79e2e0
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-20 13:12:32 -0300

    feat(house_widget): add animation on init

--- lib/modules/widgets/house_widget.dart
+++ lib/modules/widgets/house_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class House extends StatefulWidget {
   final String name;
-  final Function(void)? onTap;
+  final Function()? onTap;
   const House({Key? key, required this.name, this.onTap}) : super(key: key);
 
   @override
@@ -10,19 +10,48 @@ class House extends StatefulWidget {
 }
 
 class _HouseState extends State<House> with TickerProviderStateMixin {
+  bool done = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initLoading();
+  }
+
+  void initLoading() async {
+    await Future.delayed(Duration(milliseconds: 1300));
+    setState(() {
+      done = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Center(
-        child: AnimatedSize(
-          curve: Curves.easeOut,
-          duration: Duration(milliseconds: 300),
-          vsync: this,
-          child: Container(
-            margin: const EdgeInsets.all(8),
-            alignment: Alignment.center,
-            color: Colors.red,
-            child: Text(widget.name),
+        child: InkWell(
+          onTap: widget.onTap,
+          child: AnimatedSize(
+            curve: Curves.easeOut,
+            duration: Duration(milliseconds: 300),
+            vsync: this,
+            child: SizedBox(
+              width: done ? null : 0,
+              height: done ? null : 0,
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: Container(
+                  margin: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Theme.of(context).accentColor)),
+                  alignment: Alignment.center,
+                  child: Text(
+                    widget.name,
+                    style: TextStyle(color: Theme.of(context).accentColor),
+                  ),
+                ),
+              ),
+            ),
           ),
         ),
       ),
