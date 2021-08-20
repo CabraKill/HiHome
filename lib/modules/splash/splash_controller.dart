7805c5f271a6718d725bb8e10eb0f42fa1929351
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-20 10:35:46 -0300

    Add route change to splash controller

--- lib/modules/splash/splash_controller.dart
+++ lib/modules/splash/splash_controller.dart
@@ -1,3 +1,15 @@
 import 'package:get/get.dart';
+import 'package:hihome/routes/routes.dart';
 
-class SplashController extends GetxController {}
+class SplashController extends GetxController {
+  @override
+  void onReady() {
+    super.onReady();
+    initLoading();
+  }
+
+  void initLoading() async {
+    await Future.delayed(Duration(seconds: 1));
+    Get.offNamed(Routes.HOME);
+  }
+}
