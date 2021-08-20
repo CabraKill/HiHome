2187120047283e9991b1af963800200e113e13d9
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-20 10:38:16 -0300

    Move house widget. Update House widget with model Dependency injection

---
+++ lib/modules/widgets/house_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class House extends StatefulWidget {
+  final String name;
+  final Function(void)? onTap;
+  const House({Key? key, required this.name, this.onTap}) : super(key: key);
+
+  @override
+  _HouseState createState() => _HouseState();
+}
+
+class _HouseState extends State<House> with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: AnimatedSize(
+          curve: Curves.easeOut,
+          duration: Duration(milliseconds: 300),
+          vsync: this,
+          child: Container(
+            margin: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            color: Colors.red,
+            child: Text(widget.name),
+          ),
+        ),
+      ),
+    );
+  }
+}
