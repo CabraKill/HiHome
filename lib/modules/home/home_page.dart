f38edeee6a68533825a7296799452b71a705e9c2
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-20 13:12:07 -0300

    feat(homePage): add obx to page and onTap to houses

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hihome/modules/widgets/houseBase_widget.dart';
-import 'package:hihome/widgets/house_widget.dart';
+import 'package:hihome/modules/widgets/house_widget.dart';
 import 'home_controller.dart';
 
 class HomePage extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Obx(() => Scaffold(
         appBar: AppBar(title: Text('HomePage')),
         body: SafeArea(
             child: Center(
-          child: AspectRatio(
-            aspectRatio: 1.4,
-            child: Column(
-              children: [
-                Expanded(
-                    child: Row(
-                        children: controller.houseList
-                            .map((house) => House(name: house.name))
-                            .toList())),
-                HouseBase()
-              ],
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: AspectRatio(
+              aspectRatio: 1.4,
+              child: Column(
+                children: [
+                  Expanded(
+                      child: Row(
+                          children: controller.houseList
+                              .map((house) => House(
+                                    name: house.name,
+                                    onTap: () =>
+                                        controller.goToDetails(house.id),
+                                  ))
+                              .toList())),
+                  HouseBase()
+                ],
+              ),
             ),
           ),
-        )));
+        ))));
   }
 }
