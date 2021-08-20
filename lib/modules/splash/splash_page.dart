05c8b84abaa498dc2a22fc8655585699cf8f3567
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-20 10:36:21 -0300

    Remove house widgets from splash page

--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hihome/modules/splash/widgets/houseBase_widget.dart';
-import 'package:hihome/modules/splash/widgets/house_widget.dart';
 import 'splash_controller.dart';
 
 class SplashPage extends GetView<SplashController> {
@@ -14,25 +12,7 @@ class SplashPage extends GetView<SplashController> {
           child: SizedBox(
               width: MediaQuery.of(context).size.width * 0.7,
               child: Center(
-                child: AspectRatio(
-                  aspectRatio: 1.4,
-                  child: Column(
-                    children: [
-                      Expanded(
-                          child: Row(
-                        children: [
-                          House(
-                            key: ValueKey('1'),
-                          ),
-                          House(
-                            key: ValueKey('2'),
-                          ),
-                        ],
-                      )),
-                      HouseBase()
-                    ],
-                  ),
-                ),
+                child: CircularProgressIndicator(),
               )),
         )));
   }
