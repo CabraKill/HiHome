b453f35b9cf39a7f73783b2553f63a4d9433fad9
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-24 12:27:52 -0300

    feat(splashBidingi): remove database service init

--- lib/modules/splash/splash_biding.dart
+++ lib/modules/splash/splash_biding.dart
@@ -1,13 +1,9 @@
 import 'package:get/get.dart';
-import 'package:hihome/data/provider/firebase/firebaseSDK.dart';
-import 'package:hihome/data/repository/dataBase_repository.dart';
-
 import 'splash_controller.dart';
 
 class SplashBinding implements Bindings {
   @override
-  void dependencies() {
-    Get.put(DataBaseRepository(FirestoreSDK()), permanent: true);
+  void dependencies() async {
     Get.put<SplashController>(SplashController());
   }
 }
