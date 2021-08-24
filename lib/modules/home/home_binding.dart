b262277206a2f261ec0bfec2a7303deecff9bfa4
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-24 12:26:50 -0300

    feat(homeBinding): update database import

--- lib/modules/home/home_binding.dart
+++ lib/modules/home/home_binding.dart
@@ -1,7 +1,4 @@
 import 'package:get/get.dart';
-import 'package:hihome/data/provider/firebase/mock/firebaseMock.dart';
-import 'package:hihome/data/repository/dataBase_repository.dart';
-
 import 'home_controller.dart';
 
 class HomeBinding implements Bindings {
