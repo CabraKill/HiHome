246ef184504c1512d6b1952608f6a2ce485e67b4
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-25 10:12:23 -0300

    feat(deviceModel): add id and name

--- lib/data/models/device.dart
+++ lib/data/models/device.dart
@@ -1,13 +1,20 @@
 class DeviceModel {
-  String state;
+  final String id;
+  final String state;
+  final String name;
 
-  DeviceModel({required this.state});
+  DeviceModel({required this.id, required this.name, required this.state});
 
-  DeviceModel.fromJson(Map<String, dynamic> json) : state = json['state'];
+  DeviceModel.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        state = json['state'];
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['state'] = this.state;
+    data['id'] = id;
+    data['state'] = state;
+    data['name'] = name;
     return data;
   }
 }
