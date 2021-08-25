4eedf3c641e190db97ac0c717ae04bdff788c818
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-25 09:08:50 -0300

    feat(homePage): Reduce  duration in home

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -11,7 +11,7 @@ class HomePage extends GetView<HomeController> {
         appBar: AppBar(title: Text('HomePage')),
         body: SafeArea(
             child: Obx(() => AnimatedSwitcher(
-                  duration: Duration(milliseconds: 1000),
+                  duration: Duration(milliseconds: 300),
                   transitionBuilder: (child, animation) => SizeTransition(
                     child: child,
                     sizeFactor: animation,
