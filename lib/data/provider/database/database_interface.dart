3d7e643aecf479ff0aac71d9b3a6d679ee75c2ca
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-25 10:12:47 -0300

    feat(dataBase): add getDeviceList method

--- lib/data/provider/database/database_interface.dart
+++ lib/data/provider/database/database_interface.dart
@@ -1,6 +1,8 @@
+import 'package:hihome/data/models/device.dart';
 import 'package:hihome/data/models/house.dart';
 
 abstract class DatabasePlatform {
   Future<DatabasePlatform> init();
   Future<List<HouseModel>> getHomeList();
+  Future<List<DeviceModel>> getDeviceList(String homeId);
 }
