9ed2f3d8f5bb420d738a671148d99f82d3332c96
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-25 13:04:23 -0300

    feat(database): set database with API

--- lib/data/provider/database/database.dart
+++ lib/data/provider/database/database.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:hihome/data/models/device/device.dart';
 import 'package:hihome/data/models/house.dart';
+import 'databaseAPI.dart';
 import 'databaseSDK.dart';
 import 'database_interface.dart';
 
@@ -20,6 +21,8 @@ class DataBase implements DatabasePlatform {
 
   DatabasePlatform platformChooser() {
     if (kIsWeb || Platform.isAndroid || Platform.isIOS) return FirestoreSDK();
+    if (Platform.isLinux || Platform.isWindows || Platform.isMacOS)
+      return DataBaseAPI();
     throw UnimplementedError("Platform not implemented");
   }
 
