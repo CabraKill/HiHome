a2805dcd571154d6d2ae6b4decdd7c7576d4b3d1
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-26 09:05:55 -0300

    feat(databaseAPI): correct login. Implement getHomeList

--- lib/data/provider/database/databaseAPI.dart
+++ lib/data/provider/database/databaseAPI.dart
@@ -17,21 +17,25 @@ class DataBaseAPI extends GetConnect implements DatabasePlatform {
         'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$key',
         '{"email": "raphaeldesouza@outlook.com","password":"123456","returnSecureToken": true}',
         headers: {'Content-Type': 'application/json'});
-    final jsonMap = json.decode(responseAuth.body);
+    final jsonMap = responseAuth.body;
     token = jsonMap['idToken'];
     return this;
   }
 
   @override
   Future<List<DeviceModel>> getDeviceList(String homeId) async {
-    final url = baseUrll + "/houses";
-    // final response = get(url);
     throw UnimplementedError();
   }
 
   @override
-  Future<List<HouseModel>> getHomeList() {
-    // TODO: implement getHomeList
-    throw UnimplementedError();
+  Future<List<HouseModel>> getHomeList() async {
+    final url = baseUrll + "/documents/houses?mask.fieldPaths=name";
+    final response =
+        await get(url, headers: {'Authorization': 'Bearer $token'});
+    final houseList = response.body['documents']
+        .map<HouseModel>((document) => HouseModel.fromJson(document['fields']
+          ..['id'] = (document['name'] as String).split('/').last))
+        .toList();
+    return houseList;
   }
 }
