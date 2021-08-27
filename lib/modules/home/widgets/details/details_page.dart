20127e256297e52a11f32eb2b9c8260ec243f754
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-27 16:37:38 -0300

    feat(detailsPage): add offSetHeight to construtor

--- lib/modules/home/widgets/details/details_page.dart
+++ lib/modules/home/widgets/details/details_page.dart
@@ -5,37 +5,37 @@ import 'package:hihome/modules/home/widgets/details/bulb_widget.dart';
 import 'details_controller.dart';
 
 class DetailsPage extends StatelessWidget {
+  final double offSetHeight;
+
+  const DetailsPage({Key? key, required this.offSetHeight}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return GetBuilder<DetailsController>(
       init: DetailsController(),
       builder: (controller) => Stack(
         children: [
-          Expanded(
-            child: LayoutBuilder(builder: (context, contraints) {
-              return Obx(() {
-                return Stack(
-                  children: [
-                    ...controller.itens,
-                    AnimatedPositioned(
-                      duration: Duration(milliseconds: 300),
-                      child: Container(
-                        width: contraints.maxWidth * 10,
-                        height: contraints.maxHeight * 10,
-                        margin: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color:
-                                    Theme.of(context).colorScheme.secondary)),
-                        alignment: Alignment.center,
-                        child: Text("oi"),
-                      ),
+          LayoutBuilder(builder: (context, contraints) {
+            return Obx(() {
+              return Stack(
+                children: [
+                  ...controller.itens,
+                  AnimatedPositioned(
+                    duration: Duration(milliseconds: 300),
+                    child: Container(
+                      width: contraints.maxWidth * 10,
+                      height: contraints.maxHeight * 10,
+                      margin: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Theme.of(context).colorScheme.secondary)),
+                      alignment: Alignment.center,
+                      child: Text("oi"),
                     ),
-                  ],
-                );
-              });
-            }),
-          ),
+                  ),
+                ],
+              );
+            });
+          }),
           Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
@@ -53,8 +53,9 @@ class DetailsPage extends StatelessWidget {
                       final widget = BulbWidget(
                         xPosition: data.offset.dx -
                             MediaQuery.of(context).padding.left,
-                        yPosition:
-                            data.offset.dy - MediaQuery.of(context).padding.top,
+                        yPosition: data.offset.dy -
+                            MediaQuery.of(context).padding.top -
+                            offSetHeight,
                       );
                       controller.addDeviceToList(widget);
                       print(controller.itens);
