208e172ba3e20a533623f8a9ad8f24d0c8451c5f
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-27 06:20:08 -0300

    feat(databaseAPI): refactor init with token, getHomeList and add getRoomList with UnimplementedError for now

--- lib/data/provider/database/databaseAPI.dart
+++ lib/data/provider/database/databaseAPI.dart
@@ -1,24 +1,29 @@
-import 'dart:convert';
-
 import 'package:get/get_connect.dart';
 import 'package:hihome/data/models/house.dart';
 import 'package:hihome/data/models/device/device.dart';
+import 'package:hihome/data/models/room.dart';
 import 'package:hihome/data/provider/database/database_interface.dart';
+import 'package:hihome/data/provider/request/connectionClient.dart';
 
 class DataBaseAPI extends GetConnect implements DatabasePlatform {
   static const baseUrll =
       "https://firestore.googleapis.com/v1/projects/home-dbb7e/databases/(default)";
-  late String token;
+  // late String token;
   static const key = 'AIzaSyBSHy46Cn5RRlP3qcuwwsCzXsP1PYVhApo';
+  final ConnectionClient connectionClient;
+
+  DataBaseAPI(this.connectionClient);
 
   @override
   Future<DatabasePlatform> init() async {
+    //TODO: remove this login hard coded
     final responseAuth = await post(
         'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$key',
         '{"email": "raphaeldesouza@outlook.com","password":"123456","returnSecureToken": true}',
         headers: {'Content-Type': 'application/json'});
     final jsonMap = responseAuth.body;
-    token = jsonMap['idToken'];
+    final String token = jsonMap['idToken'];
+    connectionClient.defaultHeaders['Authorization'] = 'Bearer $token';
     return this;
   }
 
@@ -29,13 +34,18 @@ class DataBaseAPI extends GetConnect implements DatabasePlatform {
 
   @override
   Future<List<HouseModel>> getHomeList() async {
-    final url = baseUrll + "/documents/houses?mask.fieldPaths=name";
-    final response =
-        await get(url, headers: {'Authorization': 'Bearer $token'});
-    final houseList = response.body['documents']
+    final route = "/documents/houses?mask.fieldPaths=name";
+    final response = await connectionClient.get(route);
+    final houseList = response.bodyJson['documents']
         .map<HouseModel>((document) => HouseModel.fromJson(document['fields']
           ..['id'] = (document['name'] as String).split('/').last))
         .toList();
     return houseList;
+  }
+
+  @override
+  Future<List<RoomModel>> getRoomList(String homeId) async {
+    // final response = connectionClient.get('/documents/$homeId/rooms');
+    throw UnimplementedError();
   }
 }
