b535894b2b66c686a6466a6cf1fd0c74a4c80d5d
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-27 06:30:51 -0300

    fix(connectionClient): remove const defaultHeaders

--- lib/data/provider/request/connectionClient.dart
+++ lib/data/provider/request/connectionClient.dart
@@ -4,12 +4,14 @@ import 'package:hihome/data/provider/request/client.dart';
 class ConnectionClient {
   final String baseUrl;
   final Client client;
-  final Map<String, dynamic> defaultHeaders;
+  late Map<String, dynamic> defaultHeaders;
 
   ConnectionClient(
       {required this.client,
       required this.baseUrl,
-      this.defaultHeaders = const {}});
+      Map<String, dynamic>? defaultHeaders}) {
+    this.defaultHeaders = defaultHeaders ?? {};
+  }
 
   Future<ResponseModel> get(String route,
       {Map<String, dynamic>? headers, bool useDefaultHeaders = true}) {
