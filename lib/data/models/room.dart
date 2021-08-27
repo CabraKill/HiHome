d2b83a152cc2d8bda6c3d79ac1ce2ec4a632e7a3
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-27 14:12:27 -0300

    feat(room): fix id and anme deserialization

--- lib/data/models/room.dart
+++ lib/data/models/room.dart
@@ -8,8 +8,8 @@ class RoomModel {
   RoomModel({required this.id, required this.name, this.deviceList});
 
   RoomModel.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        name = json['name']['stringValue'];
+      : id = (json['name'] as String).split('/').last,
+        name = json['fields']['name']['stringValue'];
 
   // Map<String, dynamic> toJson() {
   //   final Map<String, dynamic> data = new Map<String, dynamic>();
