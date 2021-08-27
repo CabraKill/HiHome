716064b6885a7b7229809273240a767a999d0d06
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-27 06:13:58 -0300

    feat(connectionClient): add get logic and improve contructor

--- lib/data/provider/request/connectionClient.dart
+++ lib/data/provider/request/connectionClient.dart
@@ -1,10 +1,22 @@
 import 'package:hihome/data/models/response.dart';
+import 'package:hihome/data/provider/request/client.dart';
 
 class ConnectionClient {
   final String baseUrl;
-  final client;
+  final Client client;
+  final Map<String, dynamic> defaultHeaders;
 
-  ConnectionClient(this.client, this.baseUrl);
+  ConnectionClient(
+      {required this.client,
+      required this.baseUrl,
+      this.defaultHeaders = const {}});
 
-  Future<Response> get() {}
+  Future<ResponseModel> get(String route,
+      {Map<String, dynamic>? headers, bool useDefaultHeaders = true}) {
+    final requestHeaders = {
+      if (useDefaultHeaders) ...defaultHeaders,
+      ...headers ?? {}
+    };
+    return client.getRequest(baseUrl + route, requestHeaders);
+  }
 }
