0cb00afb5c829d728f342e9b2e5e348d33bbe8c9
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-30 16:37:09 -0300

    feat(loginController): add login

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,10 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hihome/data/helper/loginError_type.dart';
+import 'package:hihome/domain/Ilogin_usecase.dart';
 
 class LoginController extends GetxController {
-// final MyRepository repository;
-  LoginController();
+  final ILoginUseCase loginUseCase;
+  LoginController(this.loginUseCase);
 
-  final _obj = ''.obs;
-  set obj(value) => this._obj.value = value;
-  get obj => this._obj.value;
+  final loginFieldController = TextEditingController();
+  final passwordFieldController = TextEditingController();
+
+  void login() async {
+    final result = await loginUseCase(
+        loginFieldController.text, passwordFieldController.text);
+    result.fold(
+        (failure) => showLoginFailure(failure),
+        (loginResult) => Get.defaultDialog(
+            title: "Success",
+            content: Text(
+              "Token: ${loginResult.token.substring(0, 10) + "..."}",
+            )));
+  }
+
+  void showLoginFailure(LoginFailureType failure) {
+    Get.defaultDialog(title: "Error", content: Text(failure.description));
+  }
 }
