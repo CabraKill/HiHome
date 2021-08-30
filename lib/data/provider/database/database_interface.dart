430fa153b0c22a11485111e567f7a7b31f23a20d
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-30 16:27:09 -0300

    feat(DatabasePlatform): remove datz from interface

--- lib/data/provider/database/database_interface.dart
+++ lib/data/provider/database/database_interface.dart
@@ -1,6 +1,4 @@
-import 'package:dartz/dartz.dart';
 import 'package:hihome/data/models/device/device.dart';
-import 'package:hihome/data/models/failure.dart';
 import 'package:hihome/data/models/house.dart';
 import 'package:hihome/data/models/loginResult.dart';
 import 'package:hihome/data/models/room.dart';
@@ -8,7 +6,7 @@ import 'package:hihome/data/models/room.dart';
 abstract class DatabasePlatform {
   ///Init the database dependencies
   Future<DatabasePlatform> init();
-  Future<Either<Failure, LoginResult>> login(String email, String password);
+  Future<LoginResult> login(String email, String password);
   Future<List<HouseModel>> getHomeList();
   Future<List<RoomModel>> getRoomList(String homeId);
   Future<List<DeviceModel>> getDeviceList(String homeId);
