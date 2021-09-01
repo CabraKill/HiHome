54934e5df43dc8dc9addee29ea8ab3a2051a34dd
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-09-01 16:13:00 -0300

    feat: create database repository

---
+++ lib/data/repositories/database_repository.dart
@@ -0,0 +1,33 @@
+import 'package:get/get.dart';
+import 'package:hihome/data/models/family.dart';
+import 'package:hihome/data/models/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:hihome/data/models/house.dart';
+import 'package:hihome/data/provider/database/database.dart';
+import 'package:hihome/domain/repositories/database_repository.dart';
+
+class DatabaseRepository implements IDatabaseRepository {
+  final DataBase dataBase;
+
+  DatabaseRepository() : dataBase = Get.find();
+
+  @override
+  Future<Either<Failure, FamilyModel>> getFamily() async {
+    try {
+      final result = await dataBase.getFamily();
+      return Right(result);
+    } catch (e) {
+      return Left(Failure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<HouseModel>>> getHouseList() async {
+    try {
+      final result = await dataBase.getHomeList();
+      return Right(result);
+    } catch (e) {
+      return Left(Failure(e.toString()));
+    }
+  }
+}
---
+++ lib/domain/repositories/database_repository.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:hihome/data/models/failure.dart';
+import 'package:hihome/data/models/family.dart';
+import 'package:hihome/data/models/house.dart';
+
+abstract class IDatabaseRepository {
+  //TODO: specify failure
+  Future<Either<Failure, FamilyModel>> getFamily();
+  //TODO: specify failure
+  Future<Either<Failure, List<HouseModel>>> getHouseList();
+}
