f4ae976e8421fbb495cf3ba2d52bfae6bd2a5b06
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-09-01 14:53:21 -0300

    feat(loginRepository): change user model class name

--- lib/domain/repositories/login_repository.dart
+++ lib/domain/repositories/login_repository.dart
@@ -1,8 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:hihome/data/helper/auth_error/loginFailure_type.dart';
-import 'package:hihome/data/models/user.dart';
+import 'package:hihome/data/models/userCredentials.dart';
 
 abstract class ILogingRepository {
-  Future<Either<LoginFailureType, UserModel>> login(
+  Future<Either<LoginFailureType, UserCredentials>> login(
       String email, String password);
 }
