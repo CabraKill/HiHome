44dae8f9ff8d98be5aa55e0c00e3d28b2cc2e1a2
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-09-01 14:57:22 -0300

    feat(homeBinding): inject userDetailsRepository in homeController instance

--- lib/modules/home/home_binding.dart
+++ lib/modules/home/home_binding.dart
@@ -1,9 +1,10 @@
 import 'package:get/get.dart';
+import 'package:hihome/data/repositories/userDetails_repository.dart';
 import 'home_controller.dart';
 
 class HomeBinding implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<HomeController>(() => HomeController());
+    Get.lazyPut<HomeController>(() => HomeController(UserDetailsRepository()));
   }
 }
