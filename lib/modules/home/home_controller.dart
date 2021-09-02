4243a0e542af2b92206054b13a75edb7c602ac22
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-09-02 16:33:36 -0300

    feat(homeController): add error passing

--- lib/modules/home/home_controller.dart
+++ lib/modules/home/home_controller.dart
@@ -51,7 +51,7 @@ class HomeController extends GetxController with StateMixin, ErrorDialog {
     final result = await databaseRepository.getHouseList();
     result.fold(
       (failure) {
-        houseListState(HomeState.error);
+        houseListState(HomeState.error, error: failure.toString());
         // houseListState.error.value = failure.toString();
       },
       (houseList) {
