234617f3fdba2fa0a0d9e722f6046ca5f3809a55
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-09-03 10:05:22 -0300

    feat(authError): change location import

--- lib/data/helper/auth_error/invalidPassword_error.dart
+++ lib/data/helper/auth_error/invalidPassword_error.dart
@@ -1,4 +1,4 @@
-import 'package:hihome/data/helper/auth_error/auth_error.dart';
+import 'package:hihome/data/helper/connection_erro/auth_error.dart';
 
 class InvalidPasswordException extends AuthException {
   InvalidPasswordException([String? text]) : super(text);
