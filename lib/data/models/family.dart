feda1e24b8483bb307104cfbb29208eda65ab905
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-09-03 13:53:12 -0300

    fea(family): add fromJson

--- lib/data/models/family.dart
+++ lib/data/models/family.dart
@@ -6,4 +6,13 @@ class FamilyModel {
   List<HouseModel>? houseList;
 
   FamilyModel({required this.documentName, required this.name, this.houseList});
+
+  FamilyModel.fromJson(Map<String, dynamic> jsonMap)
+      : documentName = (jsonMap['name'] as String).split('/').last,
+        name = jsonMap['fields']['name']['stringValue'],
+        houseList = jsonMap['fields']['houses'] != null
+            ? (jsonMap['fields']['houses'] as List<Map<String, dynamic>>)
+                .map((house) => HouseModel.fromJson(house))
+                .toList()
+            : null;
 }
