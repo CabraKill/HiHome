6603e9b1a94d2321ba60c3f0fc6f7d66a5180819
cabrakill-pc-o <raphaeldesouza@outlook.com>
2021-09-13 16:16:29 -0300

    feat(getUnit_usecase): implementing usecase

---
+++ lib/data/usecases/getUnit_usecase.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:get/get.dart';
+import 'package:hihome/data/models/failure.dart';
+import 'package:hihome/data/models/family.dart';
+import 'package:hihome/domain/repositories/database_repository.dart';
+import 'package:hihome/domain/usecases/getUnit_usecase.dart';
+
+class GetUnitUseCase implements IGetUnitUseCase {
+  final IDatabaseRepository databaseRepository;
+
+  GetUnitUseCase() : databaseRepository = Get.find();
+
+  @override
+  Future<Either<Failure, FamilyModel>> getUnit() {
+    // TODO: implement getUnit
+    throw UnimplementedError();
+  }
+}
