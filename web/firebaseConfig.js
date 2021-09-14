49027db8e7a5088b5b6148c45287e1584c419c8f
cabrakill-pc-o <raphaeldesouza@outlook.com>
2021-09-14 09:32:15 -0300

    feat(web): add firebaseConfig to source controll

---
+++ web/firebaseConfig.js
@@ -0,0 +1,9 @@
+firebaseConfig = {
+  apiKey: "AIzaSyAIdWnqjoG0uo3Z2CYYpB0IHig1CqtLpKA",
+  authDomain: "home-dbb7e.firebaseapp.com",
+  projectId: "home-dbb7e",
+  storageBucket: "home-dbb7e.appspot.com",
+  messagingSenderId: "858269029086",
+  appId: "1:858269029086:web:75ca4762e3140b6e62284f",
+  measurementId: "G-06WTFE5PW1"
+}
