17681dfe7d9301f03af929c755052b8e56e39355
cabrakill-pc-o <raphaeldesouza@outlook.com>
2021-09-15 14:59:31 -0300

    feat(getUnitUseCase): atualiza build version para 1.0.30+31

--- lib/data/usecases/get_unit_usecase.dart
+++ lib/data/usecases/get_unit_usecase.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:hihome/data/models/failure.dart';
 import 'package:hihome/data/models/family.dart';
 import 'package:hihome/domain/repositories/database_repository.dart';
-import 'package:hihome/domain/usecases/getUnit_usecase.dart';
+import 'package:hihome/domain/usecases/get_unit_usecase.dart';
 
 class GetUnitUseCase implements IGetUnitUseCase {
   final IDatabaseRepository databaseRepository;
