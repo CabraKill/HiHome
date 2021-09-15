76609b356e429a8e889154b0ab018fe76f7d5ddc
cabrakill-pc-o <raphaeldesouza@outlook.com>
2021-09-15 15:03:31 -0300

    feat(homeChooser): change file name to lowercase style

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hihome/modules/home/widgets/details/details_page.dart';
-import 'package:hihome/modules/home/widgets/homeChooser/homeChooser_widget.dart';
+import 'package:hihome/modules/home/widgets/homeChooser/home_chooser_widget.dart';
 import 'home_controller.dart';
 
 class HomePage extends GetView<HomeController> {
