4d556ce9329d44046ee1385178b9559ca2f76278
cabrakill-pc-o <raphaeldesouza@outlook.com>
2021-09-15 15:00:21 -0300

    feat(loginUseCase): atualiza build version para 1.0.30+31

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hihome/data/helper/auth_error/login_failure_type.dart';
 import 'package:hihome/data/models/user_credentials.dart';
-import 'package:hihome/domain/usecases/Ilogin_usecase.dart';
+import 'package:hihome/domain/usecases/ilogin_usecase.dart';
 import 'package:hihome/routes/routes.dart';
 
 class LoginController extends GetxController {
