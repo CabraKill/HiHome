a3a8f561ef63626eea77b56e61183854cc8cd172
cabrakill-pc-o <raphaeldesouza@outlook.com>
2021-09-15 14:55:58 -0300

    feat(userDetails): change file name to lowercase style

--- lib/modules/home/home_binding.dart
+++ lib/modules/home/home_binding.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 import 'package:hihome/data/repositories/database_repository.dart';
-import 'package:hihome/data/repositories/userDetails_repository.dart';
+import 'package:hihome/data/repositories/user_details_repository.dart';
 import 'home_controller.dart';
 
 class HomeBinding implements Bindings {
