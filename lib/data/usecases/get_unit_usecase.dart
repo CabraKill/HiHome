33f25076f6fc597e6a6cbf6b40090792a1cc6b09
cabrakill-pc-o <raphaeldesouza@outlook.com>
2021-09-16 16:00:59 -0300

    feat(getUnitUseCase): changing flow

--- lib/data/usecases/get_unit_usecase.dart
+++ lib/data/usecases/get_unit_usecase.dart
@@ -8,11 +8,18 @@ import 'package:hihome/domain/usecases/get_unit_usecase.dart';
 class GetUnitUseCase implements IGetUnitUseCase {
   final IDatabaseRepository databaseRepository;
 
-  GetUnitUseCase() : databaseRepository = Get.find();
+  GetUnitUseCase(this.databaseRepository);
 
+  //TODO: create specific failure
   @override
-  Future<Either<Failure, FamilyModel>> getUnit() {
-    // TODO: implement getUnit
-    throw UnimplementedError();
+  Future<Either<Failure, FamilyModel>> getUnit(String familyId) async {
+    final homeResult = await databaseRepository.getFamily(familyId);
+    late FamilyModel home;
+    homeResult.fold((failure) => failure, (homeData) => home = homeData);
+    // final homeResult = await databaseRepository.getHouseList(familyId);
+    if (homeResult is Right<Failure, FamilyModel>) home = homeResult.value;
+    if (homeResult is Left<Failure, FamilyModel>) return Left(homeResult.value);
+    // final homeResult = await databaseRepository.get(familyId);
+    homeResult.fold((failure) => failure, (homeData) => home = homeData);
   }
 }
--- lib/domain/usecases/get_unit_usecase.dart
+++ lib/domain/usecases/get_unit_usecase.dart
@@ -3,5 +3,5 @@ import 'package:hihome/data/models/failure.dart';
 import 'package:hihome/data/models/family.dart';
 
 abstract class IGetUnitUseCase {
-  Future<Either<Failure, FamilyModel>> getUnit();
+  Future<Either<Failure, FamilyModel>> getUnit(String familyId);
 }
