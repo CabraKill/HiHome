601e893375de4e9ed26b663b81625c9093350d07
Raphael Souza <raphaeldesouza@outlook.com>
2021-10-25 18:59:00 -0300

    feature: implement GetSectionListUseCase

--- lib/data/usecases/get_section_list_usecase.dart
+++ lib/data/usecases/get_section_list_usecase.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:hihome/data/models/failure.dart';
+import 'package:hihome/domain/models/device.dart';
 import 'package:hihome/domain/models/section.dart';
 import 'package:hihome/domain/repositories/database_repository.dart';
 import 'package:hihome/domain/usecases/get_section_list_usecase.dart';
@@ -10,7 +11,26 @@ class GetSectionListUseCaseImpl implements GetSectionListUseCase {
   GetSectionListUseCaseImpl(this.databaseRepository);
 
   @override
-  Future<Either<Failure, List<SectionEntity>>> call(String unitId) {
+  Future<Either<Failure, List<SectionEntity>>> call(String unitId) async {
+    final sectionListResult = await databaseRepository.getSectionList(unitId);
+    late List<SectionEntity> sectionList;
+    if (sectionListResult is Left<Failure, List<SectionEntity>>) {
+      return Left(sectionListResult.value);
+    }
+
+    sectionList = (sectionListResult as Right<Failure, List<SectionEntity>>).value;
+
+    dynamic error;
+    sectionList.forEach((section) async {
+      final deviceListResult =
+          await databaseRepository.getDeviceList(section.path);
+      if (deviceListResult is Left<Failure, List<DeviceEntity>>) {
+        error = deviceListResult.value;
+        return;
+      }
+      section.deviceList = (deviceListResult as Right).value;
+    });
+    if (error != null) return Left(error);
     return databaseRepository.getSectionList(unitId);
   }
 }
