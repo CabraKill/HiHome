44bb4218eece9d544e8411e6e3953f8995f63e73
Raphael Souza <raphaeldesouza@outlook.com>
2021-10-25 20:00:37 -0300

    feature: add app bar and division

--- lib/modules/home/subPage/details/details_page.dart
+++ lib/modules/home/subPage/details/details_page.dart
@@ -13,62 +13,80 @@ class DetailsPage extends GetView<DetailsController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
+      appBar: appBar,
+        body: Column(
+          children: [
+            Stack(
       children: [
-        LayoutBuilder(builder: (context, contraints) {
-          return Obx(() => Stack(
-                children: [
-                  ...controller.itens
-                      .map<DeviceWidget>((device) => DeviceWidget(
-                            device: device,
-                            offset: Offset(0, offSetHeight),
-                            key: ValueKey(device.id + device.point.x.toString() + device.point.y.toString()),
-                          )),
-                  Container(
-                    width: contraints.maxWidth * 10,
-                    height: contraints.maxHeight * 10,
-                    margin: const EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Theme.of(context).colorScheme.secondary)),
-                    alignment: Alignment.center,
-                  ),
-                ],
-              ));
-        }),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                const Icon(Icons.check_box_outline_blank),
-                Draggable<DeviceModel>(
-                  child: const Icon(Icons.lightbulb),
-                  feedback: const Icon(Icons.lightbulb),
-                  childWhenDragging: Icon(Icons.lightbulb,
-                      color: Theme.of(context).colorScheme.secondary),
-                  onDragEnd: (data) {
-                    //TODO: make them relative
-                    final dx = data.offset.dx;
-                    final dy = data.offset.dy;
-                    final device = DeviceModel(
-                        id: '0',
-                        name: '',
-                        state: '',
-                        point: DevicePointModel(x: dx, y: dy));
-                    controller.addDeviceToList(device);
-                    debugPrint(controller.itens.toString());
-                  },
+            LayoutBuilder(builder: (context, contraints) {
+              return Obx(() => Stack(
+                    children: [
+                      ...controller.itens
+                          .map<DeviceWidget>((device) => DeviceWidget(
+                                device: device,
+                                offset: Offset(0, offSetHeight),
+                                key: ValueKey(device.id +
+                                    device.point.x.toString() +
+                                    device.point.y.toString()),
+                              )),
+                      Container(
+                        width: contraints.maxWidth * 10,
+                        height: contraints.maxHeight * 10,
+                        margin: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Theme.of(context).colorScheme.secondary)),
+                        alignment: Alignment.center,
+                      ),
+                    ],
+                  ));
+            }),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    const Icon(Icons.check_box_outline_blank),
+                    Draggable<DeviceModel>(
+                      child: const Icon(Icons.lightbulb),
+                      feedback: const Icon(Icons.lightbulb),
+                      childWhenDragging: Icon(Icons.lightbulb,
+                          color: Theme.of(context).colorScheme.secondary),
+                      onDragEnd: (data) {
+                        //TODO: make them relative
+                        final dx = data.offset.dx;
+                        final dy = data.offset.dy;
+                        final device = DeviceModel(
+                            id: '0',
+                            name: '',
+                            state: '',
+                            point: DevicePointModel(x: dx, y: dy));
+                        controller.addDeviceToList(device);
+                        debugPrint(controller.itens.toString());
+                      },
+                    ),
+                    const Icon(Icons.water),
+                    const Icon(Icons.precision_manufacturing),
+                  ],
                 ),
-                const Icon(Icons.water),
-                const Icon(Icons.precision_manufacturing),
-              ],
+              ),
             ),
-          ),
-        ),
       ],
-    ));
+    ),
+          ],
+        ));
   }
+
+  AppBar get appBar => AppBar(
+        title: const Text('HomePage'),
+        actions: [
+          IconButton(
+              onPressed: (){
+                //TODO: implement on the controller
+              },
+              icon: const Icon(Icons.update))
+        ],
+      );
 }
