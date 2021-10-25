848a72a3277aa6a4f68f2e0475e51eb302ed0f2c
Raphael Souza <raphaeldesouza@outlook.com>
2021-10-25 18:46:43 -0300

    feature(DatabaInterface): change section to entity

--- lib/data/provider/database/database_interface.dart
+++ lib/data/provider/database/database_interface.dart
@@ -1,9 +1,9 @@
 import 'package:hihome/data/models/unit.dart';
-import 'package:hihome/data/models/section.dart';
 import 'package:hihome/data/models/room.dart';
 import 'package:hihome/data/models/user.dart';
 import 'package:hihome/data/models/user_credentials.dart';
 import 'package:hihome/domain/models/device.dart';
+import 'package:hihome/domain/models/section.dart';
 
 abstract class DatabasePlatform {
   ///Init the database dependencies
@@ -11,7 +11,7 @@ abstract class DatabasePlatform {
   Future<UserCredentials> login(String email, String password);
   Future<UserEntity> getUser(String uid);
   Future<UnitModel> getUnit(String familyId);
-  Future<List<SectionModel>> getHomeList(String familyId);
+  Future<List<SectionEntity>> getSectionList(String familyId);
   Future<List<RoomModel>> getRoomList(String familyId, String homeId);
   Future<List<DeviceEntity>> getDeviceList(String path);
 }
