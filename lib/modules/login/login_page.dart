5f1d4b4a34fabc0bc04d9e8adcc5e36cd110e9ae
Raphael Souza <raphaeldesouza@outlook.com>
2021-10-25 18:51:02 -0300

    feature(login): add constraints to screen

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -16,13 +16,16 @@ class LoginPage extends GetView<LoginController> {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
+              margin: const EdgeInsets.all(8),
               decoration: BoxDecoration(
                   border: Border.all(
                 width: 1,
                 color: primaryColor,
               )),
-              constraints:
-                  BoxConstraints.loose(const Size(500, double.infinity)),
+              constraints: BoxConstraints.loose(const Size(
+                500,
+                double.infinity,
+              )),
               padding: const EdgeInsets.all(8),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
