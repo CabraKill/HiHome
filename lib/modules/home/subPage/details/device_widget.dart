e360760fdeaa92777ec21541f2b5ae332ad3d804
CabraKill <raphaeldesouza@outlook.com>
2021-11-10 09:48:25 -0300

    feature: fix offset

--- lib/modules/home/subPage/details/device_widget.dart
+++ lib/modules/home/subPage/details/device_widget.dart
@@ -11,7 +11,7 @@ class DeviceWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      top: device.point.y, //- offset.dy,
+      top: device.point.y - offset.dy,
       left: device.point.x,
       child: Draggable(
         feedback: Container(),
