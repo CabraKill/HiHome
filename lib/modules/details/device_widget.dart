3bde7e1b224ab8228e920d77614bb164ff035ee8
CabraKill <raphaeldesouza@outlook.com>
2021-11-10 13:47:39 -0300

    feature: add onTap to deviceWidget

--- lib/modules/details/device_widget.dart
+++ lib/modules/details/device_widget.dart
@@ -2,30 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:hihome/data/models/device/device.dart';
 
 class DeviceWidget extends StatelessWidget {
+  const DeviceWidget({
+    Key? key,
+    required this.device,
+    required this.offset,
+    this.onTap,
+  }) : super(key: key);
+
   final DeviceModel device;
   final Offset offset;
-
-  const DeviceWidget({Key? key, required this.device, required this.offset})
-      : super(key: key);
+  final GestureTapCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     return Positioned(
       top: device.point.y - offset.dy,
       left: device.point.x,
-      child: Draggable(
-        feedback: Container(),
-        child: GestureDetector(
-            onTap: () {
-              device.point.x += 10;
-            },
-            child: const Icon(Icons.lightbulb)),
-        onDragEnd: (details) {
-          device.point.x = details.offset.dx;
-          device.point.y = details.offset.dy -
-              MediaQuery.of(context).padding.top -
-              offset.dy;
-        },
+      child: InkWell(
+        onTap: onTap,
+        child: Draggable(
+          feedback: Container(),
+          child: const Icon(Icons.lightbulb),
+          onDragEnd: (details) {
+            device.point.x = details.offset.dx;
+            device.point.y = details.offset.dy -
+                MediaQuery.of(context).padding.top -
+                offset.dy;
+          },
+        ),
       ),
     );
   }
