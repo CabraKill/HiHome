60d7766de421d0504e5ae846e225d603fbfec1f0
CabraKill <raphaeldesouza@outlook.com>
2021-11-10 13:47:58 -0300

    feature: change variable name and devices stack order

--- lib/modules/details/details_page.dart
+++ lib/modules/details/details_page.dart
@@ -13,77 +13,91 @@ class DetailsPage extends GetView<DetailsController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: appBar,
-        body: Stack(
-          children: [
-            LayoutBuilder(builder: (context, contraints) {
-              return Obx(() => Stack(
-                    children: [
-                      ...controller.itens
-                          .map<DeviceWidget>((device) => DeviceWidget(
-                                device: device,
-                                offset: Offset(0, offSetHeight),
-                                key: ValueKey(device.id +
-                                    device.point.x.toString() +
-                                    device.point.y.toString()),
-                              )),
-                      Container(
-                        width: contraints.maxWidth,
-                        height: contraints.maxHeight,
-                        margin: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color:
-                                    Theme.of(context).colorScheme.secondary)),
-                        alignment: Alignment.center,
-                      ),
-                    ],
-                  ));
-            }),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+      appBar: appBar,
+      body: Stack(
+        children: [
+          LayoutBuilder(
+            builder: (context, contraints) {
+              return Obx(
+                () => Stack(
                   children: [
-                    const Icon(Icons.check_box_outline_blank),
-                    Draggable<DeviceModel>(
-                      child: const Icon(Icons.lightbulb),
-                      feedback: const Icon(Icons.lightbulb),
-                      childWhenDragging: Icon(Icons.lightbulb,
-                          color: Theme.of(context).colorScheme.secondary),
-                      onDragEnd: (data) {
-                        //TODO: make them relative
-                        final dx = data.offset.dx;
-                        final dy = data.offset.dy;
-                        final device = DeviceModel(
-                            id: '0',
-                            name: '',
-                            state: '',
-                            point: DevicePointModel(x: dx, y: dy));
-                        controller.addDeviceToList(device);
-                        debugPrint(controller.itens.toString());
-                      },
+                    Container(
+                      width: contraints.maxWidth,
+                      height: contraints.maxHeight,
+                      margin: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                      ),
+                      alignment: Alignment.center,
                     ),
-                    const Icon(Icons.water),
-                    const Icon(Icons.precision_manufacturing),
+                    ...controller.devices.map<DeviceWidget>(
+                      (device) => DeviceWidget(
+                        device: device,
+                        offset: Offset(0, offSetHeight),
+                        onTap: () => print("oi"),
+                        key: ValueKey(
+                          device.id +
+                              device.point.x.toString() +
+                              device.point.y.toString(),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
+              );
+            },
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const Icon(Icons.check_box_outline_blank),
+                  Draggable<DeviceModel>(
+                    child: const Icon(Icons.lightbulb),
+                    feedback: const Icon(Icons.lightbulb),
+                    childWhenDragging: Icon(
+                      Icons.lightbulb,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                    onDragEnd: (data) {
+                      //TODO: make them relative
+                      final dx = data.offset.dx;
+                      final dy = data.offset.dy;
+                      final device = DeviceModel(
+                        id: '0',
+                        name: '',
+                        state: '',
+                        point: DevicePointModel(x: dx, y: dy),
+                      );
+                      controller.addDeviceToList(device);
+                      debugPrint(controller.devices.toString());
+                    },
+                  ),
+                  const Icon(Icons.water),
+                  const Icon(Icons.precision_manufacturing),
+                ],
               ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 
   AppBar get appBar => AppBar(
         title: const Text('HomePage'),
         actions: [
           IconButton(
-              onPressed: () {
-                //TODO: implement on the controller
-              },
-              icon: const Icon(Icons.update))
+            onPressed: () {
+              //TODO: implement on the controller
+            },
+            icon: const Icon(Icons.update),
+          )
         ],
       );
 }
