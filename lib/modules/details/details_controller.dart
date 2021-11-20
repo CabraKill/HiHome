91c97d15a1a02e833b70283eff3ceb4a4948fbc6
CabraKill <raphaeldesouza@outlook.com>
2021-11-19 21:34:02 -0300

    feat: add cache for zoom. Update splash page. Increse delay

--- lib/modules/details/details_controller.dart
+++ lib/modules/details/details_controller.dart
@@ -20,6 +20,7 @@ import 'package:hihome/domain/usecases/get_device_list_usecase.dart';
 import 'package:hihome/domain/usecases/get_section_list_usecase.dart';
 import 'package:hihome/domain/usecases/remove_device_usecase.dart';
 import 'package:hihome/domain/usecases/update_device_value_usecase.dart';
+import 'package:hihome/infra/simple_cache/simple_cache.dart';
 import 'package:hihome/infra/valueState/value_state.dart';
 import 'package:hihome/infra/valueState/value_state_getx.dart';
 import 'package:hihome/modules/details/models/zoom_type.dart';
@@ -84,6 +85,7 @@ class DetailsController extends GetxController {
   void onInit() {
     super.onInit();
     // updateSectionList();
+    setDefaultZoom();
     updateDeviceList();
     initUpdateDeviceListTimer();
   }
@@ -182,7 +184,7 @@ class DetailsController extends GetxController {
 
   void initUpdateDeviceListTimer() {
     timerController =
-        Timer.periodic(const Duration(milliseconds: 500), (timer) {
+        Timer.periodic(const Duration(milliseconds: 1200), (timer) {
       updateDeviceList();
     });
   }
@@ -232,6 +234,7 @@ class DetailsController extends GetxController {
 
   void nextZoom() {
     deviceZoom = deviceZoom.next;
+    SimpleCache.instance.updateValue('device_zoom', deviceZoom.index);
   }
 
   void updatePoint(DeviceEntity device, DevicePointModel point) async {
@@ -242,6 +245,11 @@ class DetailsController extends GetxController {
       (error) => debugPrint("edit device error: $error"),
       (_) {},
     );
+  }
+
+  void setDefaultZoom() async{
+    final zoomNumber = await SimpleCache.instance.readValue('device_zoom') ?? 0;
+    deviceZoom =  DeviceZoomType.values[zoomNumber];
   }
 }
 
