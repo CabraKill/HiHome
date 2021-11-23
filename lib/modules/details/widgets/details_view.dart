353aaa29bbde858cd8bef2ba56e8ccbbc3abb1c3
CabraKill <raphaeldesouza@outlook.com>
2021-11-22 21:26:42 -0300

    feat: make graph line cyan

--- lib/modules/details/widgets/details_view.dart
+++ lib/modules/details/widgets/details_view.dart
@@ -50,7 +50,7 @@ class DetailsView extends StatelessWidget {
                                   dataSource: controller.deviceAnlysisLogsRx,
                                   xValueMapper: (
                                     DeviceLogEntity log,
-                                    _,
+                                    index,
                                   ) =>
                                       "${log.date.hour}:${log.date.minute}:${log.date.second}",
                                   yValueMapper: (
@@ -65,6 +65,18 @@ class DetailsView extends StatelessWidget {
                                     isVisible: true,
                                   ),
                                 )
+                              ],
+                              palette: const <Color>[
+                                Colors.cyan,
+                                Color.fromRGBO(192, 108, 132, 1),
+                                Color.fromRGBO(246, 114, 128, 1),
+                                Color.fromRGBO(248, 177, 149, 1),
+                                Color.fromRGBO(116, 180, 155, 1),
+                                Color.fromRGBO(0, 168, 181, 1),
+                                Color.fromRGBO(73, 76, 162, 1),
+                                Color.fromRGBO(255, 205, 96, 1),
+                                Color.fromRGBO(255, 240, 219, 1),
+                                Color.fromRGBO(238, 238, 238, 1)
                               ],
                             ),
                             Align(
